cbf3d2565a0c29fa829d08702c26a2713660e18e
sufian <sufian4se@gmail.com>
2022-11-25 19:46:03 +0600

    firebase config file added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>Resale app</h1>
+      <h1 className="text-3xl">Resale app</h1>
+      <button className="btn btn-active">Button</button>
+<button className="btn btn-active bg-primary">Button</button>
+<button className="btn btn-active btn-secondary">Button</button>
+<button className="btn btn-active btn-accent">Button</button>
+<button className="btn btn-active btn-ghost">Button</button>
+<button className="btn btn-active bg-gradient-to-r from-primary to-secondary">Button</button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,24 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui:{
+    themes: [
+      {
+        resaletheme:{
+          primary: '#d62828',
+          secondary: '#0E032E',
+          accent: "#3A4256",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+        }
+      }
+    ]
+  },
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
 }
+
+
+// #0E032E #E82527 d62828
